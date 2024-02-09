7b650b3c67066fc8fe30b1b88f693bcc23b11503
Nataliia Balabukha <153378052+NATPOWERS23@users.noreply.github.com>
2024-02-09 16:48:00 +0100

    git hide enviroment files

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from './interface';
-
-export const environment: Environment = {
-  production: true,
-  apiKey: 'AIzaSyARegUZWbdSLJXyd5bC6tkEtwCS6WYCVjE',
-  fbDbUrl: 'https://bird-store-default-rtdb.europe-west1.firebasedatabase.app/',
-};
